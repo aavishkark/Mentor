a4a4a39f23f7396e460e1873df3a5b8645a364a6
Avishkar Kamble <aavishkark@gmail.com>
2025-10-23 08:12:53 +0530

    Implement Companion Component using vapi

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -4,6 +4,7 @@ import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import { getSubjectColor } from '@/lib/utils';
 import Image from 'next/image';
+import CompanionComponent from '@/components/CompanionComponent';
 
 interface CompanionSessionPageProps {
   params: Promise<{ id: string} >
@@ -11,7 +12,8 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async ({params}: CompanionSessionPageProps) => {
   const {id} = await params;
-  const {name, subject, title, topic, duration} = await getCompanion(id);
+  const companion = await getCompanion(id);
+  const { name, subject, title, topic, duration } = companion;
   const user = await currentUser();
 
   if(!user) redirect('/sign-in');
@@ -48,6 +50,12 @@ const CompanionSession = async ({params}: CompanionSessionPageProps) => {
           {duration} minutes
         </div>
       </article>
+      <CompanionComponent 
+        {...companion}
+        compaanionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+      />
     </main>
   );
 };
