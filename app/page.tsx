faa263254e420ce7e605139aa095884d478540ce
Avishkar Kamble <aavishkark@gmail.com>
2025-11-14 20:07:32 +0530

    Implement profile page

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,33 @@
-import React from 'react';
+export const dynamic = 'force-dynamic';
+
 import MentorTile from '@/components/MentorTile/MentorTile';
 import MentorList from '@/components/MentorsList/MentorList';
 import Cta from '@/components/Cta/Cta';
 import './home.css';
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.action';
+import { getSubjectColor } from '@/lib/utils';
 
-export const recentSessions = [
-  {
-    id: "1",
-    subject: "science",
-    name: "Neura the Brainy Explorer",
-    topic: "Neural Network of the Brain",
-    duration: 45,
-    color: "#E5D0FF",
-  },
-  {
-    id: "2",
-    subject: "maths",
-    name: "Countsy the Number Wizard",
-    topic: "Derivatives & Integrals",
-    duration: 30,
-    color: "#FFDA6E",
-  },
-  {
-    id: "3",
-    subject: "language",
-    name: "Verba the Vocabulary Builder",
-    topic: "English Literature",
-    duration: 30,
-    color: "#BDE7FF",
-  },
-  {
-    id: "4",
-    subject: "coding",
-    name: "Codey the Logic Hacker",
-    topic: "Intro to If-Else Statements",
-    duration: 45,
-    color: "#FFC8E4",
-  },
-  {
-    id: "5",
-    subject: "history",
-    name: "Memo, the Memory Keeper",
-    topic: "World Wars: Causes & Consequences",
-    duration: 15,
-    color: "#FFECC8",
-  },
-  {
-    id: "6",
-    subject: "economics",
-    name: "The Market Maestro",
-    topic: "The Basics of Supply & Demand",
-    duration: 10,
-    color: "#C8FFDF",
-  },
-];
+const Home = async() => {
 
-export default function Home() {
+  const mentors = await getAllCompanions( { limit:3 } );
+  const rescentSessionsMentors = await getRecentSessions(10);
+  
   return (
     <main>
       <h1 className='text-2xl'>Your Mentors</h1>
       <section className='home-sec'>
-        <MentorTile 
-          id ="123"
-          name = "Neura the Brainy Explorer"
-          topic = "Neural Network of the Brain"
-          subject = "Science"
-          duration = {45}
-          color = "#ffda6e"
-        />
-        <MentorTile
-          id ="124"
-          name = "Countsy of the Number Wizard"
-          topic = "Derivatives & Integrals"
-          subject = "Science"
-          duration = {30}
-          color = "#e5d0ff"
-        />
-        <MentorTile 
-          id ="789"
-          name = "Verba the Vocabulary Builder"
-          topic = "Language"
-          subject = "English Literature"
-          duration = {20}
-          color = "#bde7ff"
-        />
+        {mentors.map((mentor)=>(
+            <MentorTile 
+              key= {mentor.id}
+              {...mentor}
+              color={getSubjectColor(mentor.subject)}
+            />
+          ))}
       </section>
       <section className='home-sec'>
         <MentorList 
           title = "Recently Completed Sessions"
-          companions= {recentSessions}
+          companions= {rescentSessionsMentors}
           classNames = "w-f2/3 max-lg:w-full"
         />
         <Cta/>
@@ -96,3 +35,4 @@ export default function Home() {
     </main>
   )
 }
+export default Home;
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,73 @@
-import React from 'react';
 
-const ProfilePage = () => {
+import './profile.css';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import { getUserCompanions, getUserSessions } from '@/lib/actions/companion.action';
+import Image from 'next/image';
+import MentorList from "@/components/MentorsList/MentorList";
+
+const ProfilePage = async() => {
+  const user = await currentUser();
+  if(!user) redirect('/sign-in');
+
+  const companions = await getUserCompanions(user.id);
+  const sessionHistory = await getUserSessions(user.id);
   return (
-    <div>Profile</div>
+    <main className="profile">
+      <section className="profile-header">
+        <div className="profile-user">
+          <Image src={user.imageUrl} alt={user.firstName!} width={110} height={110} />
+          <div className="profile-info">
+            <h1>{user.firstName} {user.lastName}</h1>
+            <p>{user.emailAddresses[0].emailAddress}</p>
+          </div>
+        </div>
+
+        <div className="profile-stats">
+          <div className="stat-card">
+            <div className="stat-row">
+              <Image src="/icons/check.svg" alt="checkmark" width={22} height={22} />
+              <p className="stat-number">{sessionHistory.length}</p>
+            </div>
+            <div className="stat-label">Lessons Completed</div>
+          </div>
+
+          <div className="stat-card">
+            <div className="stat-row">
+              <Image src="/icons/cap.svg" alt="cap" width={22} height={22} />
+              <p className="stat-number">{companions.length}</p>
+            </div>
+            <div className="stat-label">Mentors Created</div>
+          </div>
+        </div>
+      </section>
+
+      <Accordion type="multiple">
+        <AccordionItem value="recent">
+          <AccordionTrigger className="accordion-title">
+            My Sessions ({sessionHistory.length})
+          </AccordionTrigger>
+          <AccordionContent>
+            <MentorList title="Recent Sessions" companions={sessionHistory} />
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="companions">
+          <AccordionTrigger className="accordion-title">
+            My Mentors ({companions.length})
+          </AccordionTrigger>
+          <AccordionContent>
+            <MentorList title="My Companions" companions={companions} />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </main>
   );
 };
 
